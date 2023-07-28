e0533d3fd1b7f43bc971f475913028de1ee46ba2
Marselina-you <marselina88@list.ru>
2023-07-28 08:10:43 +0300

    подключение скриптов

---
+++ wp-content/themes/auto/footer.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+  <div class="container">
+    <div class="footer__inner">
+      <a class="logo" href="#">
+        <img class="logo__img" src="img/logo.svg" alt="logo">
+      </a>
+
+      <div class="social footer__social">
+        <a class="social__link" href="#">
+          <img class="social__img" src="img/icon/instagram.svg" alt="instagram icon">
+        </a>
+        <a class="social__link" href="#">
+          <img class="social__img" src="img/icon/telegram.svg" alt="telegram icon">
+        </a>
+        <a class="social__link" href="#">
+          <img class="social__img" src="img/icon/whatsapp.svg" alt="whatsapp icon">
+        </a>
+        <a class="social__link" href="#">
+          <img class="social__img" src="img/icon/facebook.svg" alt="facebook icon">
+        </a>
+      </div>
+
+      <a class="footer__copy" href="#">
+        Политика конфиденциальности
+      </a>
+    </div>
+  </div>
+</footer>
+<?php wp_footer(); ?>
---
+++ wp-content/themes/auto/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="theme-color" content="#111111">
+  <title>Auto</title>
+<?php wp_head(); ?>
+
+  <!--<link rel="stylesheet" href="css/vendor.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+  <script defer src="js/main.js"></script>
+-->
+
+</head>
+
+
+
+<body>
+
+  <header class="header">
+  <div class="container">
+    <div class="header__top">
+      <a class="logo" href="#">
+        <img class="logo__img" src="img/logo.svg" alt="logo">
+      </a>
+      <a class="phone" href="tel:+380505556677">+38 (050) 555 66 77</a>
+    </div>
+    <div class="header__content">
+      <h1 data-wow-delay=".5s" class="header__title wow animate__fadeInLeft">
+        авто из сша «под ключ»
+      </h1>
+      <h2 data-wow-delay="1s" class="header__subtitle wow animate__fadeInLeft">
+        ЗАКАЖИ СЕБЕ АВТО ИЗ США С ВЫГОДОЙ ДО 40%
+      </h2>
+      <p data-wow-delay="1.5s" class="header__text wow animate__fadeInLeft">
+        Подбор, покупка, доставка, растаможка, ремонт, оформление по договору
+      </p>
+      <a class="button" href="#">КОНСУЛЬТАЦИЯ ЭКСПЕРТА</a>
+      <div class="social header__social">
+        <a class="social__link" href="#">
+          <svg class="test" width="26" height="26">
+            <use xlink:href="img/icon/sprite.svg#instagram"></use>
+          </svg>
+        </a>
+        <a class="social__link" href="#">
+          <svg width="25" height="19">
+            <use xlink:href="img/icon/sprite.svg#telegram"></use>
+          </svg>
+        </a>
+        <a class="social__link" href="#">
+          <svg width="26" height="26">
+            <use xlink:href="img/icon/sprite.svg#whatsapp"></use>
+          </svg>
+        </a>
+        <a class="social__link" href="#">
+          <svg width="14" height="25">
+            <use xlink:href="img/icon/sprite.svg#facebook"></use>
+          </svg>
+        </a>
+      </div>
+      <img data-wow-delay="2s" class="header__img wow animate__fadeInUpBig" src="img/ford-mustang.png"
+        alt="ford mustang">
+    </div>
+  </div>
+</header>
